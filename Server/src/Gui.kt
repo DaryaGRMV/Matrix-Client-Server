c4b41c0cf93d12fdac539c23f10c6745780fa1a6
DaryaGRMV <gromovadarya99@gmail.com>
2020-05-17 12:16:21 +0500

    upd Gui

--- Server/src/Gui.kt
+++ Server/src/Gui.kt
@@ -1,19 +1,18 @@
 package ru.dargr
 
 import java.net.Socket
+import java.util.*
 import javax.swing.JFrame
 import javax.swing.JPanel
 import javax.swing.JTextArea
 
 
-class Gui(server: Server?){
+class Gui(private val server: Server) {
     //текстовое поле, куда выводится результат
     private val mainTextArea: JTextArea? = null
     private val scroll: JPanel? = null
-    private var server: Server? = null
 
-    init{
-        this.server = server
+    init {
         val frame = JFrame("Result area")
         frame.contentPane = scroll
         frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
@@ -21,37 +20,15 @@ class Gui(server: Server?){
         frame.isVisible = true
         frame.setSize(640, 720)
         frame.isAlwaysOnTop = true
-
     }
-
-    /*fun Gui(server: Server?) {
-        this.server = server
-        val frame = JFrame("Result area")
-        frame.contentPane = scroll
-        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        frame.pack()
-        frame.isVisible = true
-        frame.setSize(640, 720)
-        frame.isAlwaysOnTop = true
-    }*/
-
     /*
     * synchronized означает, что если два или более потока не могут использовать этот метод одновременно
-    * вместо этого они вызовут этот метод по очереди.
-    * это нужно на случай, если несколько клиентов завершат вычисление почти одновременно -
-    * без synchronized в поле вывода несколько результатов бы перемешались между собой
-    * */
+    * вместо этого они вызовут этот метод по очереди.*/
     @Synchronized
-    fun returnResult(socket: Socket, result: String?) { /*
-        * получили от подключения результат вычисления - вывели его в поле,
-        * а само подключение вернули серверу как "освободившееся",
-        * то есть готовое взять на себя очередную вычислительную задачу
-        * */
+    fun returnResult(socket: Socket?, result: String?) {
+        /* получили от подключения результат вычисления - вывели его в поле,
+        * а само подключение вернули серверу как "освободившееся"*/
         mainTextArea!!.append(result)
-        server?.returnSocket(socket)
-    }
-
-    fun append(s: String?) {
-        mainTextArea!!.append(s)
+        server.returnSocket(socket!!)
     }
 }
