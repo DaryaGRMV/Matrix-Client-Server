43d3f6779def873829f51b7e21a8dd9d81c39e56
DaryaGRMV <gromovadarya99@gmail.com>
2020-05-17 12:01:45 +0500

    upd ResultConnection.kt

--- Server/src/ResultConnection.kt
+++ Server/src/ResultConnection.kt
@@ -1,39 +1,25 @@
 package ru.dargr
 
 import java.io.IOException
-import java.io.InputStream
 import java.net.Socket
 
+//Этот класс используется для приемы результата вычислений от клиента
 
-class ResultConnection(resultSocket: Socket, gui: Gui): Runnable {
-    /*
-* Этот класс используется для приемы результата вычислений от клиента
-* одна вычислительная задача - один поток, ждущий ответ
-* */
-    private var socket: Socket? = null
-    private var gui: Gui? = null
-
-    fun ResultConnection(socket: Socket?, gui: Gui?) {
-        this.socket = socket
-        this.gui = gui
-    }
-
+class ResultConnection(private val socket: Socket, private val gui: Gui) : Runnable {
     override fun run() {
         try {
-            val inputStream: InputStream = socket!!.getInputStream()
-
+            val inputStream = socket.getInputStream()
+            var c : Char
             while (!Character.isAlphabetic(inputStream.read())) {
                 Thread.sleep(100)
             }
-
-            var c: Char = inputStream.read().toChar()
-
+            c = inputStream.read().toChar()
             val resultBuilder = StringBuilder()
             while (c != '$') {
                 resultBuilder.append(c)
                 c = inputStream.read().toChar()
             }
-            gui!!.returnResult(socket!!, resultBuilder.toString())
+            gui.returnResult(socket, resultBuilder.toString())
         } catch (e: InterruptedException) {
             e.printStackTrace()
         } catch (e: IOException) {
